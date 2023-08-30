ea89ed828f474dd3425baadb37ccc5ee623312c3
prakassa <brighter.prakas@gmail.com>
2023-08-30 13:36:14 +0300

    if or else

---
+++ Kimmo's first lecture.py
@@ -0,0 +1,9 @@
+x = int(input("How much cash you have?"))
+#str = "you have {}cash"
+#print(str.format(x))
+print(f"you said that you have{x}")
+
+if(x>10):
+    print("you have more than 10 euros")
+else:
+    print("you poor bastard")
