0392a0ebf4e42f5bdff5c52408a796bde994506a
prakassa <brighter.prakas@gmail.com>
2023-09-09 17:49:35 +0300

    Inch to cm

--- 4.2 inch to cm.py
+++ 4.2 inch to cm.py
@@ -1,16 +1,12 @@
 
 print("convert inches to cms")
-inch = float(input("How many inches is it?"))
 
-cm= inch*2.54
+while True:
+        inch = float(input("How many inches is it?"))
+        if inch < 0:
+                print("Program end")
 
-if inch > 0:
-        count=int(input("how many times should i display?"))
-        i=0
-        while i < count:
+        cm= inch*2.54
+
+        if inch > 0:
                 print("Ok, that will be", cm, "centimeters")
-                i=i+1
-if inch < 0:
-        print("Program end")
-else:
-        print("Thank you for asking")
