3cd71e2eb74c470a9cc48805062ca1572b14c891
prakassa <brighter.prakas@gmail.com>
2023-09-17 15:10:49 +0300

    5.3 Prime number check

---
+++ 5.3prime numbers.py
@@ -0,0 +1,19 @@
+print("Check if the number is prime number. Press '0' to stop.")
+number = int(input("Enter your number"))
+while number != 0:
+    if number == 1:
+        print(f"no {number} is not a prime number")
+    if number == 0:
+        print(f"no {number} is not a prime number")
+    elif number < 1:
+        for i in range(2,number):
+            if number % i == 0:
+                print(f"no {number} is not a prime number ")
+                break
+
+    else:
+        print(f"yes {number} is a prime number")
+
+    number = int(input("Enter your number"))
+
+print("Thank you! program stopped")
