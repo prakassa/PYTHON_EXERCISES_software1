508fbae107966614f7a09bea63cb1bfa4fc9bc08
prakassa <brighter.prakas@gmail.com>
2023-09-18 18:20:29 +0300

    6.4 sum list

---
+++ 6.4 sum in list.py
@@ -0,0 +1,10 @@
+
+min = int(input("enter the minimum limit"))
+max = int(input("Enter the maximum range"))
+lst = list(range(min,max+1,1))
+
+print(lst)
+def sumlst():
+    return sum(lst)
+
+print(sumlst())
